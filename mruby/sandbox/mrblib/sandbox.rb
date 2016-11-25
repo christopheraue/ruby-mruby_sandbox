6ab63a731d638980101c7269faf73880d26f7285
Christopher Aue <mail@christopheraue.net>
2016-11-25 15:00:15 +0100

    fixed namespace constants are defined under in code given to Sandbox#evaluate

--- mruby/sandbox/mrblib/sandbox.rb
+++ mruby/sandbox/mrblib/sandbox.rb
@@ -18,14 +18,21 @@ class Sandbox < WorldObject::Gate
       config = "Kernel##{config}" unless config.include? '#'
       owner, name = config.split('#')
       Object.const_get(owner).__send__(define_method, name) { client }
-    else
-      client
     end
+
+    client
   end
 
   world_public def evaluate(code, file = '', lineno = 0)
     @toplevel_binding.eval(code, nil, file, lineno)
   end
+end
+
+# This is a tautology but otherwise constants defined during evaluation of
+# the given string in Sandbox#evaluate are not defined under the root namespace
+# but under Sandbox.
+def eval(*args)
+  super
 end
 
 Sandbox.new(self).tap do |sandbox|
